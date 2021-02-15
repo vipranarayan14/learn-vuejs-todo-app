f7a694eebf5a2ac9ddd1ee756573fb128cd9ab10
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2021-02-15 17:25:56 +0530

    learn: counter

---
+++ src/components/Todos.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>{{ count }}</h2>
+    <button @click="increaseCount">Change</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todos",
+  data() {
+    return {
+      count: 0,
+    };
+  },
+
+  methods: {
+    increaseCount: function() {
+      this.count += 1;
+    },
+  },
+};
+</script>
